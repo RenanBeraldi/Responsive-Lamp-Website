46a4dacb8d97afcc304f09405693e62f04da79e0
RenanBeraldi <renanberaldi@outlook.com>
2023-09-29 00:07:37 -0300

    scroll bar and active link in nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,10 +93,27 @@ const toggleItem = (item) => {
   }
 };
 
-/*=============== SHOW SCROLL UP ===============*/
-
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((currentSection) => {
+    const sectionHeight = currentSection.offsetHeight,
+      sectionTop = currentSection.offsetTop - 58,
+      sectionId = currentSection.getAttribute("id"),
+      sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== DARK LIGHT THEME ===============*/
 
